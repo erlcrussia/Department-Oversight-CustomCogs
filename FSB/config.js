3091e07787f188fa9a952b1ab1ef071165efc9dd
Revolution-Roleplay-Servers <erlcrussiaroblox@gmail.com>
2026-06-05 20:49:03 +0300

    Фикс

---
+++ FSB/config.js
@@ -0,0 +1,7 @@
+const ICON_URL = "https://media.discordapp.net/attachments/1363556047704293517/1450075054783070279/-.png?ex=69413722&is=693fe5a2&hm=8df5ed934da0589d0104501342d2167e493c50f7a19138c0dcf5bdb18e531280&=&format=webp&quality=lossless";
+const HEX_COLOR = 0x001b5e;
+
+module.exports = {
+    ICON_URL,
+    HEX_COLOR,
+};
