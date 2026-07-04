bddb47aacbb9e114ee0dfc767dde323621f9d851
Pancakecreates <erlcrussiaroblox@gmail.com>
2026-07-04 12:33:06 +0300

    фикс

--- FSVNG/tasks/statusTask.js
+++ FSVNG/tasks/statusTask.js
@@ -1,11 +1,11 @@
 const { ActivityType } = require('discord.js');
 
 const statusList = [
-  { name: "Сотрудники ФСБ", type: ActivityType.Watching },
-  { name: "ФСБ России", type: ActivityType.Watching },
+  { name: "Сотрудники ФСВНГ", type: ActivityType.Watching },
+  { name: "ФСВНГ России", type: ActivityType.Watching },
   { name: "Жалобы и Обращения", type: ActivityType.Watching },
-  { name: "Повышения", type: ActivityType.Watching },
-  { name: "Федеральный Розыск", type: ActivityType.Watching }
+  { name: "Наказания", type: ActivityType.Watching },
+  { name: "База Данных", type: ActivityType.Watching }
 ];
 let currentIndex = 0;
 
