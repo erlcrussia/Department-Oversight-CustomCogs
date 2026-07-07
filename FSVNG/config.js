726c3da53671e27245951193ab0d1ea1382dc467
Pancakecreates <erlcrussiaroblox@gmail.com>
2026-07-07 13:43:03 +0300

    фикс ФСВНГ бота

--- FSVNG/config.js
+++ FSVNG/config.js
@@ -1,5 +1,5 @@
-const ICON_URL = "https://media.discordapp.net/attachments/1363556047704293517/1450075054783070279/-.png?ex=69413722&is=693fe5a2&hm=8df5ed934da0589d0104501342d2167e493c50f7a19138c0dcf5bdb18e531280&=&format=webp&quality=lossless";
-const HEX_COLOR = 0x001b5e;
+const ICON_URL = "https://media.discordapp.net/attachments/1363556047704293517/1524001692188676186/image.png?ex=6a4e28ae&is=6a4cd72e&hm=5b70bfa43f3ccd361b7b9a36f9b6dde64e35efc5c3b4610513ae2c2c9072626a&=&format=webp&quality=lossless";
+const HEX_COLOR = 0xbf0000;
 
 module.exports = {
     ICON_URL,
