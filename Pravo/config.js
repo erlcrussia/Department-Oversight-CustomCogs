efacff5bf0a6f50c562ef9ba20bd819b17a08436
Pancakecreates <erlcrussiaroblox@gmail.com>
2026-07-11 14:19:53 +0300

    фикс файлов

---
+++ Pravo/config.js
@@ -0,0 +1,7 @@
+const ICON_URL = "https://cdn.discordapp.com/attachments/1363556047704293517/1428846265487327302/---.png?ex=68f3fc48&is=68f2aac8&hm=6a2f4796bb1402bafda407878daa1f1b6b0e494f0f7916d9bb0c2908ecd9fdad&";
+const HEX_COLOR = 0x001cdd;
+
+module.exports = {
+    ICON_URL,
+    HEX_COLOR,
+};
---
+++ Pravo/tasks/statusTask.js
@@ -0,0 +1,22 @@
+const { ActivityType } = require('discord.js');
+
+const statusList = [
+  { name: "Судебные Операции", type: ActivityType.Watching },
+  { name: "Судебные Операции MRP", type: ActivityType.Watching }
+];
+let currentIndex = 0;
+
+module.exports = {
+  name: 'status-changer',
+  interval: 300000,
+
+  execute: (client, logger) => {
+    if (!client.user) return;
+
+    const status = statusList[currentIndex];
+    client.user.setActivity(status.name, { type: status.type });
+
+    logger.info(`👀 Статус обновлён: ${status.name} (${ActivityType[status.type]})`);
+    currentIndex = (currentIndex + 1) % statusList.length;
+  }
+};
